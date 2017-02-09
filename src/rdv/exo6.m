fed786b99904a7265accfa9161aa5e0be682d2b6
mauric <mauric_rck@hotmail.com>
2017-02-09 10:45:56 +0100

    pascal correction of exo6

--- src/rdv/exo6.m
+++ src/rdv/exo6.m
@@ -1,10 +1,7 @@
-%% ANTENNES INTELIGENTES TP
+%% ANTENNES INTELIGENTES TP FILTRE DE CAPON
 %   Vecteur optimale :
 %
-%               inv(Rib)*v(phi)    
-%   w(m) = -------------------------
-%           v(phi)'inv(Rib)v(phi)
-%           
+% ALGORITHME DE FROST
 %
 %% VARIABLES GLOBALES
 clc
@@ -24,26 +21,26 @@ global BAUD_RATE; % rapiditï¿½ de modulation des donnï¿½es transmises
 %=============================================================================
 ROLL_OFF_FACTOR=0.3;
 NOMBRE_ANTENNES=16;
-FACTEUR_SURECH=2;
+FACTEUR_SURECH=6;
 BANDWIDTH=200e3;
 DUREE_SYMBOLE=1/BANDWIDTH;
 BAUD_RATE=1/DUREE_SYMBOLE; 
 SAMPLING_FREQ=FACTEUR_SURECH*BAUD_RATE;
 d_sur_lambda =.5;
-M = 16;
+M = NOMBRE_ANTENNES;
 
 %toujours un vecteur c'est un vecteur colonne
 
-Phis = pi/5;%20*pi/180; % dangle dï¿½incidence du signal utile
+Phis = pi/5%20*pi/180; % dangle dï¿½incidence du signal utile
 Phi1 = -30*pi/180 % angle dï¿½incidence du premier interfï¿½rent
-Phi2 = 60*pi/180 %  angale dï¿½incidence du second interfï¿½rent
-RSB = 300; % rapport de puissance (en dB) entre le signal utile
+Phi2 = +60* pi/180 %  angale dï¿½incidence du second interfï¿½rent
+RSB = 10; % rapport de puissance (en dB) entre le signal utile
 % et le bruit au niveau de chaque capteur
-RSI1 = 300 % rapport de puissance (en dB) entre le signal utile et lï¿½interfï¿½rent n?1
-RSI2 = 300 % rapport de puissance (en dB) entre le signal utile et lï¿½interfï¿½rent n?2
+RSI1 = 800 % rapport de puissance (en dB) entre le signal utile et lï¿½interfï¿½rent n?1
+RSI2 = 800 % rapport de puissance (en dB) entre le signal utile et lï¿½interfï¿½rent n?2
 %% CONSTRUCTION OF VECTEUR OPTIMALE
  
-nombre_points = 200;    %nombre de points de la discretization
+nombre_points = 500;    %nombre de points de la discretization
 phi = linspace(-pi/2,pi/2,nombre_points); 
 v = zeros(M,1);%
 vs = zeros(M,1);%
@@ -57,56 +54,68 @@ for m = 1:size(v,1)
  v2(m) = M_const*exp(-1i*2*pi*d_sur_lambda*(m-1)*sin(Phi2));
  vs(m) = M_const*exp(-1i*2*pi*d_sur_lambda*(m-1)*sin(Phis));
 end  
-Rib = ((M/RSI2).*v1*v1'+(M/RSI2).*v2*v2'+(1/RSB).*eye(M));
+Rib = ((M/RSI1).*v1*v1'+(M/RSI2).*v2*v2'+(1/RSB).*eye(M));
 Rib_inv = inv(Rib);
-w = (Rib_inv*vs)/(vs'*Rib_inv*vs);
- 
-for i = 1:size(phi,2)
-    for m = 1:size(v,1) 
-     v(m) = M_const*exp(-1i*2*pi*d_sur_lambda*(m-1)*sin(phi(i))); %li:i complex 
-    end  
-    C(i) = w'*v*v'*w;
-end
+P = [vs v1 v2];
+f = [1 0 0]';
+wfixe = P*(P'*P)*f';
+Q = I - P*inv(P'*P)*P'
 
 
-%plot some data
+
+
+%% plot some data
 figure(); 
-plot(phi,(C),'LineWidth',1);
+plot(phi,abs(C),'LineWidth',1);
 grid on
-legend('C(phi)', 'Location'ss, 'SouthEast');
+legend('C(phi)', 'Location', 'SouthEast');
 xlabel('\phi');
 ylabel('|C(\phi)|');
 title('Diagramme de Rayonnement');
 
 figure();
-TracePolar(phi,(C), -50);
+TracePolar(phi,abs(C), -50);
 legend('C(phi)', 'Location', 'SouthEast');
 title('Diagramme de Rayonnement');
 
-loglog(phi,abs(C),'-b','LineWidth',1);
+semilogy(phi,abs(C),'-b','LineWidth',1);
 grid on
 legend('C(phi)', 'Location', 'SouthEast');
 xlabel('log \phi');
 ylabel('log |C(\phi)|');    
-title('Diagramme de Rayonnement (log-log)');
-%% GENERATION DE SIGNALES
+title('Diagramme de Rayonnement (log)');
+%% GENERATION DE SIGNAUX
 
 %=============================================================================
-% 2- Gï¿½nï¿½ration des signaux sur lï¿½ensemble des capteurs
+% 2- Generation de signaux pour l´ensemble des capteurs
 %=============================================================================
 
-[MatriceR,MatriceS,x,BinaireIn,PenteSCurve] = ...
-    GeneSignaux(Phis,Phi1,Phi2,RSB,RSI1,RSI2);
-
+[MatriceR,MatriceS,x,BinaireIn,PenteSCurve] = GeneSignaux(Phis,Phi1,Phi2,RSB,RSI1,RSI2);
+mu u= 0.003;
 y = zeros(1,size(x,2));
-for id =1:size(x,2)
-    y(id) = w'* x(:,id);
+for n =1:size(x,2)
+    
+    y(n) = w'* x(:,n);
+    w(n) = Q*(w(n) - 2*mu*x(n)*conj(y(n)))+ wfixe;
+    
+    %tous les 200 echantillonges
+    % for i = 1:size(phi,2)
+%     for m = 1:size(v,1) 
+%      v(m) = M_const*exp(-1i*2*pi*d_sur_lambda*(m-1)*sin(phi(i))); %li:i complex 
+%     end  
+%     C(i) = w'*v*v'*w;
+% end
+    
+%tous les 200 echantillon
+ %diagramme de l'oeil pour l' entree
+ %eyediagram(x(1,1000:10000),FACTEUR_SURECH) ;
+% %diagramme de l'oeil pour la sortie
+ %eyediagram(y(1000:10000),FACTEUR_SURECH);   
+    
+    
 end
 
-%diagramme de l'oeil pour l' entree
- eyediagram(x(1,:),FACTEUR_SURECH) ;
-% %diagramme de l'oeil pour la sortie
- eyediagram(y,FACTEUR_SURECH);
+
 
 %% RAPPORT  PUISSANCE SIGNAl - BRUIT
 
@@ -118,20 +127,14 @@ aa = 0.73625;
 bb = 0.7375;
 Ps_out = a*a+b*b;
 Ps_in  = aa*aa+bb*bb;
-
 G = Ps_out/Ps_in
-
-%% Calcul du vecteur de pondÃ©rations optimales
-
-
-
-
+rapport = 10*log10(Ps_out/Ps_in)
 
 %% DOCUMENTATION
 
 h = get(0,'children');
 for i=length(h):-1:1
-  saveas(h(i), ['exo3_' num2str(length(h)+1-i)], 'png');
+  saveas(h(i), ['exo4_' num2str(length(h)+1-i)], 'png');
 end
 
 
